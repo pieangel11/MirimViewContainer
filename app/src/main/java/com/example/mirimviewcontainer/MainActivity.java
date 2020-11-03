4e0442a9f04496491ef49b445ff0353a27625c31
young <pieangel11@naver.com>
2020-11-03 20:08:11 +0900

    Complete ViewFlipperTest1

--- app/src/main/java/com/example/mirimviewcontainer/MainActivity.java
+++ app/src/main/java/com/example/mirimviewcontainer/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.mirimviewcontainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+    ViewFlipper flipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drawer);
+        setContentView(R.layout.activity_flipper);
+        flipper = findViewById(R.id.flipper);
+        Button btnPrev = findViewById(R.id.btn_prev);
+        Button btnNext = findViewById(R.id.btn_next);
+        btnPrev.setOnClickListener(btnListener);
+        btnNext.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.btn_prev:
+                    flipper.showPrevious(); //이전 화면을 보여줌
+                    break;
+                case R.id.btn_next:
+                    flipper.showNext(); //다음 화면을 보여줌
+                    break;
+            }
+        }
+    };
 }
