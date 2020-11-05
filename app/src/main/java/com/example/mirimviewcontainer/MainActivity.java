efafaeeb3cf07da44a4c66c5561a7381d7331c82
young <pieangel11@naver.com>
2020-11-05 21:02:04 +0900

    Complete ViewFlipper ontouchListener

--- app/src/main/java/com/example/mirimviewcontainer/MainActivity.java
+++ app/src/main/java/com/example/mirimviewcontainer/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.mirimviewcontainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
     ViewFlipper flipper;
+    float downX, upX;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +21,25 @@ public class MainActivity extends AppCompatActivity {
         Button btnNext = findViewById(R.id.btn_next);
         btnPrev.setOnClickListener(btnListener);
         btnNext.setOnClickListener(btnListener);
+        flipper.setOnTouchListener(touchListener);
     }
+    //      ★★★ XML ViewFlipper 요소에 clickable = true로 반드시 설정한다.
+    View.OnTouchListener touchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent e) {
+            if(e.getAction() == MotionEvent.ACTION_DOWN){
+                downX = e.getX();
+            }else if(e.getAction() == MotionEvent.ACTION_UP){
+                upX = e.getX();
+                if(downX > upX){
+                    flipper.showNext();
+                }else if(downX < upX){
+                    flipper.showPrevious();
+                }
+            }
+            return false;
+        }
+    };
 
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
